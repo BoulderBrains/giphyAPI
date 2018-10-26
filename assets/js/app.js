4141d011449bfe844cdb27b23a8bbbf8014707b3
Joshua <joshuadstarkey@gmail.com>
2018-10-26 00:39:26 -0700

    start and stopping now working

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,19 +17,26 @@ function showMeGifs() {
 	var searchTerm = $(this).attr("data-name");
 	var giphyKey = config.giphyAPIKey;
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + giphyKey + "&limit=10";
-	
+
 	$.ajax({
 		url: queryURL,
 		method: "GET"
-	}).then(function(response) {
+	}).then(function (response) {
+		console.log(response);
 		for (var i = 0; i < response.data.length; i++) {
 			// pulling out the items I want to use from the API
 			var rating = response.data[i].rating;
-			var imgURL = response.data[i].images.downsized.url;
+			var animateURL = response.data[i].images.downsized.url;
+			var stillURL = response.data[i].images.original_still.url;
+
 			// forming an element for the return item
 			var wrapper = $("<div class='col-lg-4 col-sm-6 wrapper'>");
 			var ratingElement = $("<p>").text("Rating: " + rating);
-			var image = $("<img class='returned-image'>").attr("src", imgURL);
+			var image = $("<img class='returned-image'>").attr("src", stillURL);
+			image.attr("data-still", stillURL);
+			image.attr("data-animate", animateURL);
+			image.attr("data-state", "still");
+		
 			// adding the ratingElement paragraph and the image img to the wrapper
 			wrapper.append(image, ratingElement);
 			// rendering the wrapper to the #results-container
@@ -38,9 +45,25 @@ function showMeGifs() {
 	});
 };
 
+// This is triggering the start/stop of a gif by swapping out the src="" URLs
+function gifState() {
+	var state = $(this).attr("data-state");
+	var dataStill = $(this).attr("data-still");
+	var dataAnimate = $(this).attr("data-animate");
+	
+	if (state === "still") {
+		$(this).attr("src", dataAnimate);
+		$(this).attr("data-state", "animate");
+	} else {
+		$(this).attr("src", dataStill);
+		$(this).attr("data-state", "still");
+	}
+}
 // Listening to the document for newly generated button, 
 // on click run the showMeGifs function
 $(document).on("click", ".search-button", showMeGifs);
+// listeing to clicks on the returned images to trigger gifState and start/stop the gif
+$(document).on("click", ".returned-image", gifState);
 
 
 
